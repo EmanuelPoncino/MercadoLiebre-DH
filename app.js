9c6666909d4322befb36644c6210a53c15408149
EmanuelPoncino <emanuel.poncino@gmail.com>
2024-02-27 16:54:17 -0300

    config puerto

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const port = process.env.PORT || 5500;
 
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
 app.get("/register", (req, res) => {
@@ -15,5 +16,5 @@ app.get("/login", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
 
-const port = process.env.PORT || 5500;
+
 app.listen(port, () => console.log("servidor corriendo en el puerto 5500"));
